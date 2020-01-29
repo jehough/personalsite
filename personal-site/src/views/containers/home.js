67df29080841659110d12a6d2f629694c9ec290b
jehough <teachlife@outlook.com>
2020-01-28 20:25:05 -0500

    adds image to page and stylings

--- personal-site/src/views/containers/home.js
+++ personal-site/src/views/containers/home.js
@@ -3,9 +3,11 @@ import React, {Component} from 'react';
 export default class Home extends Component{
   render(){
     return(
-      
+      <div>
         <h1>James Hough</h1>
-
+        <img src="/20200123_164959.jpg" alt="a pic of me...my face apparently broke your computer!" id="self-image"/>
+        <p>Full stack web developer with experience in Ruby and JavaScript frameworks. Teaching analytical logic and advanced mathematics for the last 9 years gives me a strong foundation in the thought process that drives programming. Collaborating with colleagues to build curriculum for students of varied backgrounds required creativity and ingenuity that I am excited to translate into the coding world.</p>
+      </div>
     )
   }
 }
