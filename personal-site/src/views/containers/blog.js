4d4f5f8a3d65e74f7fa6cb5ae2b5846c126b2239
jehough <teachlife@outlook.com>
2020-02-01 20:09:13 -0500

    adds video link to project page

--- personal-site/src/views/containers/blog.js
+++ personal-site/src/views/containers/blog.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import Card from 'react-bootstrap/Card'
 
 export default class Projects extends Component{
   render(){
     return(
-
+      <div className="mainProjects">
         <h1>These are my projects</h1>
-
+        <Card>
+          <iframe src="https://www.youtube.com/embed/1keOmQmGdWw"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen />
+          <Card.Body>Some Text</Card.Body>
+        </Card>
+      </div>
     )
   }
 }
