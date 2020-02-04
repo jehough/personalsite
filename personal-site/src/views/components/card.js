f5b740184186cfc11e05fa8715c25be0e3220043
jehough <teachlife@outlook.com>
2020-02-04 18:38:21 -0500

    refactors card to stateless component

---
+++ personal-site/src/views/components/card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+
+const DisplayCard = () => {
+  return(
+    <Card>
+      <iframe src="https://www.youtube.com/embed/1keOmQmGdWw"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen />
+      <Card.Body>Some Text</Card.Body>
+    </Card>
+  )
+}
+
+export default DisplayCard
--- personal-site/src/views/containers/blog.js
+++ personal-site/src/views/containers/blog.js
@@ -1,18 +1,12 @@
 import React, {Component} from 'react';
-import Card from 'react-bootstrap/Card'
+import DisplayCard from '../components/card'
 
 export default class Projects extends Component{
   render(){
     return(
       <div className="mainProjects">
         <h1>These are my projects</h1>
-        <Card>
-          <iframe src="https://www.youtube.com/embed/1keOmQmGdWw"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen />
-          <Card.Body>Some Text</Card.Body>
-        </Card>
+        <DisplayCard />
       </div>
     )
   }
