b7cdc496850b68104fc3ba587cc46fc747844561
jehough <teachlife@outlook.com>
2020-02-08 11:25:53 -0500

    adds education div

--- personal-site/src/views/containers/background.js
+++ personal-site/src/views/containers/background.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 
 export default class Background extends Component{
+  constructor(){
+    super()
+    this.state = {
+      showEd: false
+    }
+  }
+
+  handleEdClick = () => {
+    const show = this.state.showEd
+    show ? show=false : show=true
+  }
   render(){
     return(
-
-        <h1>This is my Background</h1>
-
+      <div>
+        <h1>My Background</h1>
+        <div className="education" onClick={this.handleEdClick}>
+          <h3>Education</h3>
+          {this.state.showEd ? <p>hello</p>:<p>not here</p>}
+        </div>
+      </div>
     )
   }
 }
