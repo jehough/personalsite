0605061db0c9585d9ecd7f8c628595a3dd95cd6e
jehough <teachlife@outlook.com>
2020-02-15 10:11:16 -0500

    moves react-sidenav css file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import './react-sidenav.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
