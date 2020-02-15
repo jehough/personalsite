61b393c89d9c30279826dabed9f959160d20389a
jehough <teachlife@outlook.com>
2020-02-14 20:46:41 -0500

    adds info to helper

--- personal-site/src/helper.js
+++ personal-site/src/helper.js
@@ -7,19 +7,19 @@ export const projectList = [
   },
   {
     name: "Your Budget",
-    videoUrl: "https://www.youtube.com/embed/1keOmQmGdWw",
+    videoUrl: "https://www.youtube.com/embed/hkWPUVl_edw",
     githubUrl: "https://github.com/jehough/jsproject",
     description: "Simple budget app that tracks personal or professional expenses"
   },
   {
     name: "Quiz Site",
-    videoUrl: "https://www.youtube.com/embed/1keOmQmGdWw",
+    videoUrl: "https://www.youtube.com/embed/tVymSMsSUCQ",
     githubUrl: "https://github.com/jehough/quizsite",
     description: "A fully functional quiz site where teachers can create and assign quizzes for students to take online"
   },
   {
     name: "Medical Record Site",
-    videoUrl: "https://www.youtube.com/embed/1keOmQmGdWw",
+    videoUrl: "https://www.youtube.com/embed/lUynAFy7UMU",
     githubUrl: "https://github.com/jehough/rails-medrecord",
     description: "A site where doctors and patients can schedule appointments and track patient medical information"
   }
